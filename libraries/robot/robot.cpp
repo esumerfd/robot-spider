f996252861585796fced325ababe94d81d069a4f
Ed Sumerfield <esumerfd@bitbashers.org>
2025-03-06 20:08:23 -0500

    rename lib dirs and add planning comments

--- libraries/robot/robot.cpp
+++ libraries/robot/robot.cpp
@@ -5,9 +5,20 @@ Flasher flasher;
 
 void Robot::setup() {
   flasher.begin(1000);
+
+  // seed initial sequence with movements.
+
+  // init pwm
+  // init servos to default position
+  // report status of memory, devices.
+
 }
 
 void Robot::loop() {
   flasher.flash();
+
+  // check queue for operation.
+  // take next opertion step.
+
   delay(1000);
 }
